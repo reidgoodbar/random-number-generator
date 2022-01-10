b9f6660906945a5491b65874ebf2b9726bf47ee6
Reid Goodbar <reidgoodbar@Reids-Mac-mini.local>
2022-01-09 20:30:16 -0600

    cleaned up

--- src/shift_rng.py
+++ src/shift_rng.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 def normal_random(seed, count):
     random.seed(seed)
-    n_rn = [random.randrange(0,2) for _ in range(1000000)]
+    n_rn = [random.randrange(0,2) for _ in range(count)]
     return n_rn
 
 
@@ -31,19 +31,30 @@ print(update_register(1, get_register_length(3), [0,1]))
 
 # RNG loosely based on lfsr
 def lfsr(seed, count):
-    settings = seed & int('0xFFFF', 16)
+    settings = seed & int('0xFFFFFF', 16)
+    position_0 = settings & int('1', 16)
     position_1 = settings & int('0xF', 16)
     position_2 = settings & int('0xF0', 16)
     position_3 = settings & int('0xF00', 16)
     position_4 = settings & int('0xF000', 16)
+    position_5 = settings & int('0xF0000', 16)
+    position_6 = settings & int('0xF00000', 16)
 
-    register = seed >> 4*len('FFFF')
+    register = seed >> 4*len('FFFFFF')
     lfsr_rn = []
     length = get_register_length(register)
+
+    # Run LFSR to move past seed
+    for _ in range(100000):
+        register >>= 1
+        register = update_register(register, length, [position_0, position_1, position_2, position_3, position_4, position_5, position_6])
+    
+    # Build random list
     for _ in range(count):
         lfsr_rn.append(register & 1)
         register >>= 1
-        register = update_register(register, length, [position_1, position_2, position_3, position_4])
+        register = update_register(register, length, [position_1, position_2, position_3, position_4, position_5])
+    
     return lfsr_rn
 
 
@@ -56,12 +67,14 @@ def get_seed():
 
 def main():
     # seed = get_seed()
-    seed = 134521345234524342534425647256
-    count = 100000000
+    seed = 98403759082439875973490510938409
+    count = 10000
     n_rn = normal_random(seed, count)
     lfsr_rn = lfsr(seed, count)
-    # print(lfsr_rn)
+    
+    print('Classic RNG Difference Between 1s and 0s:')
     print(abs(Counter(n_rn)[1]-Counter(n_rn)[0]))
+    print('Pseudo LFSR RNG Difference Between 1s and 0s:')
     print(abs(Counter(lfsr_rn)[1]-Counter(lfsr_rn)[0]))
 
 if __name__ == "__main__":
